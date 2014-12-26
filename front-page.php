03327e8058eea174c3128a5012887b1acfb47bc6
matheus <contato@matheusgimenez.com.br>
2014-12-26 16:52:35 -0200

    front-page.php

--- front-page.php
+++ front-page.php
@@ -16,6 +16,10 @@
 get_header('home'); ?>
 	<header class="col-md-12 home">
 	</header><!-- .col-md-12 home -->
+	<div class="col-md-2 col-md-offset-5 col-xs-12" id="home-menu">
+		<a href="#" class="active"><?php _e('Search','odin'); ?></a> |
+		<a href="#" id="categories" data-toggle="modal" data-target="#categories-modal"><?php _e('Categories','odin'); ?></a>
+	</div><!-- .col-md-6 col-md-offset-4 -->
 	<form method="get" id="searchform-home" class="col-md-12" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 		<input type="text" class="col-md-12" name="s" id="s" placeholder="<?php esc_attr_e( 'Search Pirate', 'odin' ); ?>" required />
 		<div class="col-md-6 col-md-offset-2 col-xs-12">
@@ -30,8 +34,28 @@ get_header('home'); ?>
 		<div class="col-md-4 col-md-offset-4 col-xs-12">
 			<input id="submit" type="submit" class="btn btn-default btn-lg btn-tpb col-md-12 pull-left" value="<?php esc_attr_e( 'Search', 'odin' ); ?>" />
 		</div><!-- .col-md-6 col-md-offset-4 -->
-
 	</form>
+	<!-- Modal -->
+	<div class="modal fade" id="categories-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<h4 class="modal-title" id="myModalLabel"><?php _e('Categories','odin'); ?></h4>
+				</div>
+			<div class="modal-body">
+			<?php $categories = get_categories('hide_empty=0'); ?>
+			<?php foreach ($categories as $category): ?>
+			    <a href="<?php echo get_category_link($category->cat_ID); ?>">
+			    	<?php echo $category->name . ' (' .$category->category_count. ')'; ?>
+			    </a>
+			<?php endforeach; ?>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php
 //get_sidebar();
