be4d79e4d9289603feda42abd1b0d75d5355cf25
matheus <contato@matheusgimenez.com.br>
2015-01-07 16:51:35 -0200

    add footer menu

--- footer.php
+++ footer.php
@@ -13,6 +13,20 @@
 		</div><!-- #main -->
 
 		<footer id="footer" role="contentinfo">
+		    <nav class="col-md-4 col-md-offset-4 col-xs-12">
+		    	<?php
+		    	wp_nav_menu(
+		    		array(
+		    			'theme_location' => 'main-menu',
+		    			'depth' => 1,
+		    			'container' => false,
+		    			'menu_class' => 'nav navbar-nav',
+		    			'fallback_cb' => 'Odin_Bootstrap_Nav_Walker::fallback',
+		    			'walker' => new Odin_Bootstrap_Nav_Walker()
+		    			)
+		    	);
+		    	?>
+		    </nav><!-- .col-md-6 col-md-offset-4 -->
 		</footer><!-- #footer -->
 	</div><!-- .container -->
 
